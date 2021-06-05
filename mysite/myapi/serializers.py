034b4569a4f9118c731a40f76daa956346624b8b
tahakalkanli <abdullahtahakalkanli@gmail.com>
2021-06-05 17:17:42 +0300

    django project removed

--- mysite/myapi/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Hero
-
-class HeroSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Hero
-        fields = ('id', 'name', 'alias')
