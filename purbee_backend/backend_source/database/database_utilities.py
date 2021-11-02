964851cf92dc5b75eb6ff7968eec55915062cfda
Ekin <ekinn.gngr@gmail.com>
2021-11-02 17:52:15 +0300

    Some name conventions are applied.

--- purbee_backend/backend_source/database/database_utilities.py
+++ purbee_backend/backend_source/database/database_utilities.py
@@ -1,38 +1,46 @@
-def user_name_exists():
+def get_user_name(user_name):
     pass
 
 
-def mail_address_exists():
+def get_mail_address(mail_address):
     pass
 
 
-def save_new_user_to_database(user_name, mail_address, password):
+def save_new_user(user_name, mail_address, password):
     pass
 
 
 def get_password_from_user_name(user_name):
     pass
 
+
 def set_profile_picture_by_user_name(user_name, picture):
     pass
 
-def set_first_name_by_user_name(user_name, first_name):
-    pass
-
-def set_last_name_by_user_name(user_name, last_name):
-    pass
-
-def set_birth_date_by_user_name(user_name, birth_date):
-    pass
 
 def get_profile_picture_by_user_name(user_name):
     pass
 
+
+def set_first_name_by_user_name(user_name, first_name):
+    pass
+
+
 def get_first_name_by_user_name(user_name):
     pass
 
+
+def set_last_name_by_user_name(user_name, last_name):
+    pass
+
+
 def get_last_name_by_user_name(user_name):
     pass
+
+
+def set_birth_date_by_user_name(user_name, birth_date):
+    pass
+
 
 def get_birth_date_by_user_name(user_name):
     pass
--- purbee_backend/backend_source/login/login.py
+++ purbee_backend/backend_source/login/login.py
@@ -1,7 +1,7 @@
 from purbee_backend.backend_source.database.database_utilities import (
-    user_name_exists,
-    mail_address_exists,
-    save_new_user_to_database,
+    get_user_name,
+    get_mail_address,
+    save_new_user,
     get_password_from_user_name
 )
 
@@ -11,14 +11,14 @@ from login_utilities import (
 
 
 def signup(user_name, mail_address, password):
-    if user_name_exists(user_name):
+    if get_user_name(user_name):
         return -1
-    if mail_address_exists(mail_address):
+    if get_mail_address(mail_address):
         return -2
     if not password_secure(password):
         return -3
 
-    save_new_user_to_database(user_name, mail_address, password)
+    save_new_user(user_name, mail_address, password)
 
     return 0
 
