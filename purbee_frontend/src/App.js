918ef3ab7d49a4500b4861941a7915d8868a50bd
gkberkmasraff <gkberk@masraff.co>
2021-11-09 15:47:42 +0300

    login and register pages

---
+++ purbee_frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import LoginRegister from './components/login-register/login_register';
+
+function App() {
+  return (
+    <div>
+      <LoginRegister/>
+    </div>
+  );
+}
+
+export default App;
