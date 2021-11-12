87d053b088d1f1bc87dd6f2eec350982060cd2dc
Ekin <ekinn.gngr@gmail.com>
2021-11-12 22:33:13 +0300

    Login is implemented. Database is connected. Some post types and fields are implemented.

--- purbee_backend/backend_source/login/login_utilities.py
+++ purbee_backend/backend_source/login/login_utilities.py
@@ -1,7 +1,8 @@
 import re
 
+
 def password_secure(password):
-    reg = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{6,20}$"
+    reg = "^(?=.*[A-Z])(?=.*[!@#$&*.,*%+-/])(?=.*[0-9])(?=.*[a-z]).{8,}$"
     pat = re.compile(reg)
     mat = re.search(pat, password)
     if mat:
