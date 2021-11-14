4a4ef3f3e9ffa8ede2bb162979e66446e79986f4
Berkay DEMİRTAŞ <berkaydemirtass@outlook.com>
2021-11-14 23:11:22 +0300

    profile_info endpoint implemented. With this endpoint, profile photo, first name, last name etc. can be updated or profile_info of a user can be found. get_profile_page_by_user_name and update_profile_info_by_user_name database functions are implemented. Also update_profile_info and get_profile_info functions in login.py are implemented.

--- purbee_backend/backend_source/database/database_utilities.py
+++ purbee_backend/backend_source/database/database_utilities.py
@@ -17,11 +17,19 @@ def get_mail_address(user_name):
 def get_user_by_name(user_name):
     return registered_users.find_one({"user_name": user_name})
 
+def check_user_by_user_name(user_name):
+    try:
+        registered_users.find_one({"user_name": user_name})
+        return 0
+    except:
+        return 1
+
+
 def save_new_user(user_name, mail_address, password):
     if get_user_by_mail_address(mail_address):
         return 2
     try:
-        user = {"_id": user_name, "user_name": user_name, "mail_address": mail_address, "password": password, "list_of_followers": [], "list_of_followings": []}
+        user = {"_id": user_name, "user_name": user_name, "mail_address": mail_address, "password": password, "followers": [], "following": [], "profile_photo": [], "last_name": [], "first_name":[], "birth_date":[], "post_list": []}
         registered_users.insert_one(user)
     except:
         return 1
@@ -74,17 +82,32 @@ def set_birth_date_by_user_name(user_name, birth_date):
 def get_birth_date_by_user_name(user_name):
     pass
 
+def update_profile_info_by_user_name(user_name,profile_photo,bio,first_name,last_name,birth_date):
+    try:
+        registered_users.update({"user_name": user_name}, {"$set": {"profile_photo": profile_photo, "bio": bio, "first_name": first_name, "last_name": last_name, "birth_date": birth_date}})
+        return 0
+    except:
+        return 1
 
 # 0 for successful update, 1 for error during database operation
 def update_follower_and_following_lists(user_name1,user_name2):
     try:
-        list_of_followings = get_user_by_name(user_name1)["list_of_followings"]
-        list_of_followings.append(user_name2)
-        registered_users.update({"user_name":user_name1},  {"$set":{"list_of_followings":list_of_followings}})
-        list_of_followers = get_user_by_name(user_name2)["list_of_followers"]
-        list_of_followers.append(user_name1)
-        registered_users.update({"user_name": user_name2}, {"$set": {"list_of_followers": list_of_followers}})
+        following = get_user_by_name(user_name1)["following"]
+        following.append(user_name2)
+        registered_users.update({"user_name":user_name1},  {"$set":{"following":following}})
+        followers = get_user_by_name(user_name2)["followers"]
+        followers.append(user_name1)
+        registered_users.update({"user_name": user_name2}, {"$set": {"followers": followers}})
         return 0
     except:
         return 1
+
+def get_profile_page_by_user_name(user_name):
+    try:
+        user = get_user_by_name(user_name)
+        return user['profile_photo'], user['following'], user['followers'], user['first_name'], user['last_name'], user['birth_date'], user['post_list']
+    except:
+        return None
+
+
 
--- purbee_backend/backend_source/login/login.py
+++ purbee_backend/backend_source/login/login.py
@@ -1,7 +1,10 @@
 from purbee_backend.backend_source.database.database_utilities import (
     save_new_user,
     check_password_for_user_name,
-    get_user_by_name
+    get_user_by_name,
+    update_profile_info_by_user_name,
+    check_user_by_user_name,
+    get_profile_page_by_user_name
 )
 import sys
 
@@ -21,6 +24,30 @@ def sign_up(user_name, mail_address, password):
 
 def sign_in(user_name, password):
     return check_password_for_user_name(user_name, password)
+
+def update_profile_page(user_name,profile_photo,bio,first_name,last_name,birth_date):
+    if check_user_by_user_name(user_name):
+        #no such user
+        return 1
+    else:
+        code = update_profile_info_by_user_name(user_name,profile_photo,bio,first_name,last_name,birth_date)
+        if code:
+            #database error
+            return 2
+        else:
+            return 0
+
+def get_profile_page(user_name):
+    if check_user_by_user_name(user_name):
+        # no such user
+        return 1
+    else:
+        result = get_profile_page_by_user_name(user_name)
+        if not result:
+            #database error
+            return 2
+        else:
+            return result
 
 
 
