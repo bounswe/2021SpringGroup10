1160517c4788b608edd25c0d7ceba717d297e79d
Ekin <ekinn.gngr@gmail.com>
2021-11-15 17:15:52 +0300

    Bugs in /api/profile_page/ is fixed.

--- purbee_backend/backend_source/app.py
+++ purbee_backend/backend_source/app.py
@@ -1,8 +1,6 @@
-import json
 import sys
-import pymongo
-import requests
-from flask import Flask, request, Response
+
+from flask import Flask, request
 
 from purbee_backend.backend_source.login.login import (
     sign_up,
@@ -15,127 +13,103 @@ SC_FORBIDDEN = 403
 SC_SUCCESS = 200
 SC_CREATED = 201
 SC_UNAUTHORIZED = 401
-SC_BADREQUEST = 400
+SC_BAD_REQUEST = 400
 USER_NAME = ""
 USER_PASSWORD = ""
 
 app = Flask(__name__)
 
 
-@app.route('/sign_up/', methods=['POST'])
+@app.route('/api/sign_up/', methods=['POST'])
 def sign_up_endpoint():
     req = request.get_json()
-    data = {"Response Message": None}
+    data = {"response_message": None}
     status_code = None
     try:
         return_status = sign_up(**req)
     except:
-        data["Response Message"] = "Incorrect json content. (necessary fields are mail_address,user_name,password)"
-        status_code = SC_BADREQUEST
+        data["response_message"] = "Incorrect json content. (necessary fields are mail_address,user_name,password)"
+        status_code = SC_BAD_REQUEST
         return data, status_code
     if return_status == 0:
-        data["Response Message"] = "User successfully signed up."
+        data["response_message"] = "User successfully signed up."
         status_code = SC_CREATED
     elif return_status == 1:
-        data["Response Message"] = "User name already exists."
+        data["response_message"] = "User name already exists."
         status_code = SC_FORBIDDEN
     elif return_status == 2:
-        data["Response Message"] = "E-mail address already exists."
+        data["response_message"] = "E-mail address already exists."
         status_code = SC_FORBIDDEN
     elif return_status == 3:
-        data["Response Message"] = "Password is not secure enough."
+        data["response_message"] = "Password is not secure enough."
         status_code = SC_FORBIDDEN
 
     return data, status_code
 
 
-@app.route('/sign_in/', methods=['GET'])
+@app.route('/api/sign_in/', methods=['GET'])
 def sign_in_endpoint():
     req = request.get_json()
-    data = {"Response Message": None}
+    data = {"response_message": None}
     status_code = None
     try:
         return_status = sign_in(**req)
     except:
-        data["Response Message"] = "Incorrect json content. (necessary fields are user_name,password)"
-        status_code = SC_BADREQUEST
+        data["response_message"] = "Incorrect json content. (necessary fields are user_name,password)"
+        status_code = SC_BAD_REQUEST
         return data, status_code
     if return_status == 0:
+        return_dic = {"user_name": req["user_name"]}
         status_code = SC_SUCCESS
-        data["Response Message"] = "Successfully signed in."
-        data["user_name"] = req["user_name"]
+        data["response_message"] = "Successfully signed in."
+        data["data"] = return_dic
     elif return_status == 1:
+        return_dic = {"user_name": None}
         status_code = SC_UNAUTHORIZED
-        data["Response Message"] = "Credentials are incorrect"
-        data["user_name"] = None
+        data["response_message"] = "Credentials are incorrect"
+        data["data"] = return_dic
 
     return data, status_code
 
-@app.route('/profile_page/', methods=['POST','GET'])
+
+@app.route('/api/profile_page/', methods=['POST', 'GET'])
 def profile_page():
-    print("resultDict2", file=sys.stderr)
     req = request.get_json()
-    data = {"Response Message": None}
+    data = {"response_message": None}
     status_code = None
-    user_name = ""
-    profile_photo = []
-    bio = ""
-    first_name = ""
-    last_name = ""
-    birth_date = ""
-    if "user_name" in req:
+    try:
         user_name = req["user_name"]
-    if "profile_photo" in req:
-        profile_photo = req["profile_photo"]
-    if "bio" in req:
-        bio = req["bio"]
-    if "first_name" in req:
-        first_name = req["first_name"]
-    if "last_name" in req:
-        last_name = req["last_name"]
-    if "birth_date" in req:
-        birth_date = req["birth_date"]
+    except:
+        data["response_message"] = "user_name is not specified."
+        status_code = SC_BAD_REQUEST
 
     if request.method == "POST":
-        print("resultDict3", file=sys.stderr)
-        return_status = update_profile_page(user_name,profile_photo,bio,first_name,last_name,birth_date)
+        return_status = update_profile_page(user_name, req)
         if return_status == 0:
-            data["Response Message"] = "User page updated successfully."
+            data["response_message"] = "User page updated successfully."
             status_code = SC_SUCCESS
         elif return_status == 1:
-            data["Response Message"] = "No such user."
-            status_code = SC_BADREQUEST
+            data["response_message"] = "No such user."
+            status_code = SC_BAD_REQUEST
         elif return_status == 2:
-            data["Response Message"] = "Database error occurred."
+            data["response_message"] = "Database error occurred."
             status_code = SC_FORBIDDEN
 
         return data, status_code
-    resultDict = {}
+
     if request.method == "GET":
-        return_status = get_profile_page(user_name)
-        print(return_status, file=sys.stderr)
-        if return_status == 2:
-            data["Response Message"] = "Database error occurred."
+        db_return = get_profile_page(user_name)
+        if db_return == 2:
+            data["response_message"] = "Database error occurred."
             status_code = SC_FORBIDDEN
-        elif return_status == 1:
-            data["Response Message"] = "No such user."
-            status_code = SC_BADREQUEST
+        elif db_return == 1:
+            data["response_message"] = "No such user."
+            status_code = SC_BAD_REQUEST
         else:
-            resultDict['profile_photo'] = return_status[0]
-            resultDict['following'] = return_status[1]
-            resultDict['followers'] = return_status[2]
-            resultDict['first_name'] = return_status[3]
-            resultDict['last_name'] = return_status[4]
-            resultDict['birth_date'] = return_status[5]
-            resultDict['post_list'] = return_status[6]
-            resultDict['user_name'] = user_name
-            data["Response Message"] = json.dumps(resultDict)
+            data["response_message"] = "Profile page is successfully returned. "
+            data["data"] = db_return
             status_code = SC_SUCCESS
         return data, status_code
-
-
-
-
 
 
 if __name__ == '__main__':
