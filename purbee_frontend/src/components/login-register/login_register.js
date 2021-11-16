e4ac1dc2a327fe8de8ff69d5a327a8addf6f5adc
tahakalkanli <abdullahtahakalkanli@gmail.com>
2021-11-16 11:48:40 +0300

    login page is being routed to home

--- purbee_frontend/src/components/login-register/login_register.js
+++ purbee_frontend/src/components/login-register/login_register.js
@@ -2,15 +2,13 @@ import * as React from 'react';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import './styles.css'
-import { Card } from '@mui/material';
 import Link from '@mui/material/Link';
-import {Link as RouterLink}from "react-router-dom";
+import { Link as RouterLink } from "react-router-dom";
+import axios from 'axios'
 
 export default function LoginPage() {
     const [user_name, set_username] = React.useState("");
     const [password, set_password] = React.useState("");
-    const [first_name, set_first_name] = React.useState("");
-    const [last_name, set_last_name] = React.useState("");
     const [password_repeat, set_password_repeat] = React.useState("");
     const [mail_address, set_mail_address] = React.useState("");
     const [login_or_register, set_login_or_register] = React.useState("login");
@@ -23,11 +21,11 @@ export default function LoginPage() {
     const handle_login_or_register_change = (value) => set_login_or_register(value);
 
     const signin = () => {
-        // TODO
+        
     }
 
     const signup = () => {
-        // TODO
+        
     }
 
     React.useEffect(() => {
@@ -40,10 +38,10 @@ export default function LoginPage() {
                 <form className="form">
                     <img src="https://i.ibb.co/SXprk2W/2.png" style={{ width: "25%", alignSelf: "center", paddingBottom: "0.8em" }}></img>
 
-                    <h4 style={{ alignSelf: "baseline" }}>Email</h4>
+                    <h4 style={{ alignSelf: "baseline" }}>Username</h4>
                     <div style={{ height: "0.8em" }}></div>
                     <TextField
-                        id="email"
+                        id="username"
                         formControlProps={{
                             fullWidth: true
                         }}
@@ -61,13 +59,12 @@ export default function LoginPage() {
                         type="password"
                         onChange={handle_password_change}
                     />
-
-                    <Button 
-                        variant="contained" 
-                        color="primary" 
+                    <Button
+                        variant="contained"
+                        color="primary"
                         className="form__custom-button"
                         onClick={signin}>
-                        <RouterLink to="/profile-info"> Log in </RouterLink>
+                        <RouterLink to="/home"> Log in </RouterLink>
                     </Button>
                     <div style={{ height: "1em" }}></div>
                     <div style={{ backgroundColor: "#fff", alignSelf: "center" }}>
@@ -133,12 +130,12 @@ export default function LoginPage() {
                         onChange={handle_password_repeat_change}
                     />
 
-                    <Button variant="contained" color="primary" className="form__custom-button">
+                    <Button variant="contained" color="primary" className="form__custom-button" onClick={signup}>
                         <RouterLink to="/profile-info"> Sign up </RouterLink>
                     </Button>
                     <div style={{ height: "1em" }}></div>
                     <div style={{ backgroundColor: "#fff", alignSelf: "center", display: "flex", justifyContent: "space-between" }}>
-                        Already have an account? 
+                        Already have an account?
                         <Link
                             component="button"
                             variant="body2"
@@ -147,7 +144,7 @@ export default function LoginPage() {
                         </Link>
                     </div>
                 </form>
-                
+
             </div>
         )
 }
