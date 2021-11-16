25f6070d5bb11a4aea8446c50d3f7f27e02e1e33
gkberk <77738280+gkberk@users.noreply.github.com>
2021-11-16 12:06:30 +0300

    helper file it now includes api call

---
+++ purbee_frontend/helper.js
@@ -0,0 +1,11 @@
+async function apiCall(url, call_method){
+    const response = await fetch(url, {
+        method: call_method,
+        mode: "cors",
+        headers:{
+            "Content-type": "application/json"
+        },
+    });
+    const my_json = await response.json();
+    return my_json;
+}
