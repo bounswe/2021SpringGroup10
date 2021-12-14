fb64cfb26abbdea8e8523c85d057ea68e810a453
Safa Altunel <safa.altunel@eatron.com>
2021-12-14 19:56:54 +0300

    added community post type adapter and implemented necessary functions

--- android/app/src/main/java/com/example/mvvmapp/ui/community/CommunityPostTypesFragment.kt
+++ android/app/src/main/java/com/example/mvvmapp/ui/community/CommunityPostTypesFragment.kt
@@ -5,7 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.mvvmapp.R
+import com.example.mvvmapp.adapters.CommunityPostTypeAdapter
+import com.example.mvvmapp.data.CommunityPostType
+import com.example.mvvmapp.databinding.FragmentCommunityBinding
+import com.example.mvvmapp.databinding.FragmentCommunityPostTypesBinding
 
 class CommunityPostTypesFragment : Fragment() {
 
@@ -13,7 +19,20 @@ class CommunityPostTypesFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        var postTypes = mutableListOf(
+            CommunityPostType("Basketball Event"),
+            CommunityPostType("Theatre Event")
+        )
+
+        val adapter = CommunityPostTypeAdapter(postTypes)
+        val binding = DataBindingUtil.inflate<FragmentCommunityPostTypesBinding>(
+            inflater, R.layout.fragment_community_post_types, container, false)
+
+        binding.rvPostTypes.adapter = adapter
+        binding.rvPostTypes.layoutManager = LinearLayoutManager(context)
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_community_post_types, container, false)
+        return binding.root
     }
 }
