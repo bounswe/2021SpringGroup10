61522db823563d7cc920a4205c6b0189b63e44dd
gkberk <gkberk@masraff.co>
2021-12-14 12:47:59 +0300

    Post is implemented, this is the premature version.

--- purbee_frontend/src/App.js
+++ purbee_frontend/src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import LoginRegister from './components/login-register/login_register';
 import ProfileInfo from './components/login-register/profile_info';
 import Homepage from './components/homepage/homepage';
+import Post from './components/post/post';
+
 import {
     BrowserRouter,
     Routes,
@@ -21,6 +23,7 @@ function App() {
               <Route path="/" element={<LoginRegister />} />
               <Route path="/home" element={<Homepage />} />
               <Route path="/profile-info" element={<ProfileInfo />} />
+              <Route path="/post" element={<Post />} />
           </Routes>
         </BrowserRouter>
       </div>
