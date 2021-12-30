24394ab63b7292259652fbbe95d8e7166b36cc82
gkberk <gkberk@masraff.co>
2021-12-31 02:52:42 +0300

    post now takes in posts of all possible post types, it is generic. And works with the example post and post types given by backend team

--- purbee_frontend/src/components/post/mock_post.js
+++ purbee_frontend/src/components/post/mock_post.js
@@ -3,8 +3,17 @@ export const posts = {
         post_title: "NBA is so Cool!",
         fields:{
             plainTextList:[
+                {
+                    text: "hhhaaaa ",
+                    header: "ffff"
+                },
                 {header: "Welcome",
-                text: "We are pleased to see you among us! Do not hesitate to interact with any of us! Welcome :)"}
+                text: "We are pleased to see you among us! Do not hesitate to interact with any of us! Welcome :)" +
+                    "asdddd\n e are pleased to \n see you amon\n g us! Do\n  not hesitate to \n interact with any of us! We" +
+                    "adsadsasff\ne are pleased to se\n e you among u\n s! Do not \n hesitate to \n interact with any of us! We " +
+                    "ggggggggggg\n " +
+                    "sssd\n Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." +
+                    "mucjk\n "},
             ],
             locations:[
                 {header:"Our Base Location",
@@ -16,9 +25,109 @@ export const posts = {
             ],
             dateList:[
                 {header: "Post Date",
-                text: "14 December 2021"}
+                text: "14 December 2021"},
             ]
 
         }
+    },
+    "fa8da556-68b5-4d4b-9ff8-040a07e93a7a": {
+        "post_entries_dictionary_list": [
+            {
+                "header": "Event Description",
+                text: "The torunament will host The top 100 ATP players."
+            },
+            {
+                "header": "General Text",
+                text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut \n" +
+                    "labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris \n" +
+                    " nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate \n" +
+                    "velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, \n" +
+                    "sunt in culpa qui officia deserunt mollit anim id est laborum. a aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris \n" +
+                    " nisi ut aliquip ex ea commodo cons"
+            },
+            {
+                "amount": 17,
+                "currency": "tl",
+                "header": "Event Cost"
+            },
+            {
+                "amount": 1899,
+                "currency": "dollars",
+                "header": "Other Costs"
+            },
+            {
+                "header": "Event Location",
+                "location": "<some_kind_of_location_data_representation>",
+                "text": "Court number 4."
+            },
+            {
+                "header": "Another loc",
+                "location": "LOC",
+                "text": "HELLO ssefgs"
+            },
+            {
+                "header": "Event Participation",
+                "list_of_participants": []
+            },
+            {
+                "header": "Event Date",
+                "date": "22-10-2021"
+            },
+            {
+                "header": "The Other Date",
+                "date": "14-12-2021"
+            }
+        ],
+        "pictures": ["https://i.imgur.com/QiHZ0b.png", "https://i.imgur.com/owFy4b.png", "https://i.imgur.com/LGvVMb.png"
+        , "https://i.imgur.com/8WRRCb.png", "https://i.imgur.com/QNfSeb.png", "https://imgur.com/yCWExHi.png"],
+        "post_liked_user_list": [],
+        "post_owner_user_name": "eking",
+        "post_type_id": "7f621d31-3fe9-459e-ac46-7aad77ab52a1"
     }
+}
+
+export const post_types = {
+    "7f621d31-3fe9-459e-ac46-7aad77ab52a1": {
+        "parent_community_id": "example_id2",
+        "post_field_info_dictionaries_list": [
+            {
+                "field_type": "PlainText",
+                "header": "Event Description"
+            },
+            {
+                "field_type": "PlainText",
+                "header": "General Text"
+            },
+            {
+                "field_type": "Price",
+                "header": "Event Cost"
+            },
+            {
+                "field_type": "Price",
+                "header": "Other Costs"
+            },
+            {
+                "field_type": "Location",
+                "header": "Event Location"
+            },
+            {
+                "field_type": "Location",
+                "header": "Another loc"
+            },
+            {
+                "field_type": "Participation",
+                "header": "Event Participation"
+            },
+            {
+                "field_type": "Date",
+                "header": "Event Date"
+            },
+            {
+                "field_type": "Date",
+                "header": "The Other Date"
+            }
+        ],
+        "post_type_name": "Tennis Event"
+    },
+    "asd": "HAAA"
 }
--- purbee_frontend/src/components/post/post.js
+++ purbee_frontend/src/components/post/post.js
@@ -1,29 +1,89 @@
-import React from "react";
+import * as React from 'react';
 import './post.scss';
-import {posts} from './mock_post';
+import {posts, post_types} from './mock_post';
+const post_id = "fa8da556-68b5-4d4b-9ff8-040a07e93a7a"
+const post_type_id = posts[post_id]["post_type_id"]
+const post_type = post_types[post_type_id];
+let post_type_fields = post_type["post_field_info_dictionaries_list"]
+let fields = { //each field type has an array of headers
+    "PlainText": [],
+    "Price": [],
+    "Location": [],
+    "Participation": [],
+    "Images": [],
+    "Date": []
+}
+post_type_fields.map(field => {
+    fields[field["field_type"]].push(field["header"]);
+});
 
-const post = posts[1];
+const post_fields = posts[post_id]["post_entries_dictionary_list"];
+const post_likers = posts[post_id]["post_liked_user_list"]
+const post_owner = posts[post_id]["post_owner_user_name"]
 
+function textFields(){
+    return (
+        <div>
+            {fields["PlainText"].map(header => (
+                <div className="postcard__preview-txt">{text(header)}</div>
+            ))}
+        </div>
+    )
+}
+const text = (header) => post_fields.find(pf => pf["header"] === header)["text"];
+const location_text = (header) => post_fields.find(pf => pf["header"] === header)["text"]; //implement location
+const price = (header) => {
+    const f = post_fields.find(pf => pf["header"] === header);
+    return f["amount"] + " " + f["currency"];
+};
+const date = (header) => post_fields.find(pf => pf["header"] === header)["date"]
+console.log(fields["PlainText"].map(h => text(h)));
+const locations = () => {
+    let component = "";
+    const location_text = (header) => post_fields.find(pf => pf["header"] === header);
+    fields["Location"].map(header => {
+        component += (
+            <li className="tag__item play red">
+                <a href="#"><i className="fas fa-play mr-2"/>{location_text(header)}</a>
+            </li>
+        );
+    });
+    return component;
+}
+//                {fields["PlainText"].map(header => (<div className="postcard__preview-txt">{header + ": " + text(header)}</div>))}
+
+console.log(typeof fields["PlainText"])
 const Post = (data) => {
+    const [current_image, set_current_image] = React.useState(0);
+    const images = posts[post_id]["pictures"]
     return (
         <article className="postcard dark red">
             <a className="postcard__img_link" href="#">
-                <img className="postcard__img" src={post.fields.pictureList[0].text} alt="Image Title"/>
+                <img className="postcard__img" src={images[current_image]} alt="Image Title" onClick={(event) => {
+                    set_current_image((current_image+1) % images.length);
+                    console.log((current_image+1)%images.length)
+                }}/>
             </a>
             <div className="postcard__text">
-                <h1 className="postcard__title red"><a href="#">{post.post_title}</a></h1>
+                <h1 className="postcard__title red"><a href="#">{"POST TITLE!!!!!!!!!!!"}</a></h1>
                 <div className="postcard__subtitle small">
                     <time dateTime="2020-05-25 12:00:00">
-                        <i className="fas fa-calendar-alt mr-2"></i>{post.fields.dateList[0].text}
+                        <i className="fas fa-calendar-alt mr-2"/>Creator: <a href="asd">{posts[post_id]["post_owner_user_name"]}<br/></a>
+                        {fields["Date"].map(header => (<i className="postcard__preview-txt-onur">+{header + ": " + date(header)}<br/></i>))}
+                        {fields["Price"].map(header => (<i className="fas fa-calendar-alt mr-2">{"+" + header + ": " + price(header)}<br/></i>))}
+
                     </time>
                 </div>
-                <div className="postcard__bar"></div>
-                <div className="postcard__preview-txt">{post.fields.plainTextList[0].text}
-                </div>
+                <div className="postcard__bar"/>
+
+                {fields["PlainText"].map(header => (<i className="postcard__preview-txt-onur">{header + ": " + text(header)}</i>))}
+
                 <ul className="postcard__tagbox">
-                    <li className="tag__item play red">
-                        <a href="#"><i className="fas fa-play mr-2"></i>{post.fields.locations[0].header}</a>
-                    </li>
+                    {fields["Location"].map(header =>
+                        (<li className="tag__item play red">
+                            <a href="#"><i className="fas fa-play mr-2"/>{header + ": " + location_text(header)}</a>
+                        </li>))
+                    }
                 </ul>
             </div>
         </article>
