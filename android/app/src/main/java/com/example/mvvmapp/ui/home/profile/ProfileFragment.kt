53039ff9271db8e115d752a34955804f11cbd53f
Gülşah Çilingiroğlu <cilingiroglugulsah@gmail.com>
2022-01-03 16:13:43 +0300

    update profile page feature implemented
    for the update profile page feature, a page is added to app navigation, now it can be reached from the button on the profile page

--- android/app/src/main/java/com/example/mvvmapp/ui/home/profile/ProfileFragment.kt
+++ android/app/src/main/java/com/example/mvvmapp/ui/home/profile/ProfileFragment.kt
@@ -3,6 +3,7 @@ package com.example.mvvmapp.ui.home.profile
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import android.util.Log
+import androidx.navigation.findNavController
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -30,6 +31,11 @@ class ProfileFragment : Fragment(), KodeinAware {
         viewModel = ViewModelProvider(this, factory)[ProfileViewModel::class.java]
 
         binding.viewmodel = viewModel
+
+        binding.buttonUpdateProfile.setOnClickListener { view ->
+            view.findNavController().navigate(R.id.action_profileFragment_to_updateProfileFragment)
+
+        }
 
         // as we are binding a live data (ProfileViewModel.user) to layout
         // we need to define the lifecycle owner
